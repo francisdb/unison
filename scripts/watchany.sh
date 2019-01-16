73fefd05d24d75491d1b341df60bd308dd78cf8d
Arya Irani <arya.irani@gmail.com>
2019-01-16 16:25:44 -0500

    updating scripts

--- scripts/watchany.sh
+++ scripts/watchany.sh
@@ -4,6 +4,11 @@ source="$1"
 assert_command_exists () {
   if ! ( type "$1" &> /dev/null ); then
     echo "Sorry, I need the '$1' command, but couldn't find it installed." >&2
+    if [ -n "$2" ]; then
+      echo "Try installing it with \`brew install $2\`"
+    else
+      echo "Try installing it with \` brew install $1\`"
+    fi
     exit 1
   fi
 }
